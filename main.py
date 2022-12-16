3b3249224da7b72b20fb02f84ca70455b1bd22e0
Alysson Amaral <alysson.amaral@outlook.com>
2022-12-15 22:24:59 -0400

    requesting xml from url

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import xml.etree.ElementTree as et 
+import requests
 
 
-def parseXml():
+# def parseXml():
 
-    tree = et.parse("resources/sdn_advanced.xml")
-    root = tree.getroot()
+#     tree = et.parse("resources/sdn_advanced.xml")
+#     root = tree.getroot()
+#     rows = []
 
-    for child in root:
-        print(child.tag, child.attrib)
+#     for child in root:
+#         print(child.tag, child.attrib)
 
-parseXml()
+#     for ReferenceValueSets in root.iter('AliasTypeValues'):
+#         print(ReferenceValueSets.attrib)   
+
+#     for alias in root.findall("./ReferenceValueSets"):
+#         print(alias.attrib)
+        
+def loadXML():
+
+    url = 'https://www.treasury.gov/ofac/downloads/sdn.xml'
+    resp = requests.get(url)
+
+    with open('resources/sdnOfac.xml', 'wb') as f:
+        f.write(resp.content)
+
+# parseXml()
+loadXML()
