37ef4b11e557a6d64a2b97da8153da0fa99af8d9
Alysson Amaral <alysson.amaral@outlook.com>
2022-12-20 17:20:08 -0400

    build aka list

--- main.py
+++ main.py
@@ -2,15 +2,22 @@ import xml.etree.ElementTree as et
 import requests
 import pandas as pd
 
+
+def loadXML():
+
+    url = 'https://www.treasury.gov/ofac/downloads/sdn.xml'
+    resp = requests.get(url)
+
+    with open('resources/sdnOfac.xml', 'wb') as f:
+        f.write(resp.content)
+
 def parseXml():
     tree = et.parse("resources/sdnOfac.xml")
     root = tree.getroot()
     return root
 
-#building main df
 def buildMainDf():
-    tree = et.parse("resources/sdnOfac.xml")
-    root = tree.getroot()
+    root = parseXml()
  
     df_base_cols = ["uid", "lastName", "sdnType"]
     rows = []
@@ -30,8 +37,7 @@ def buildMainDf():
     return (df_base)
 
 def buildProgramList():
-    tree = et.parse("resources/sdnOfac.xml")
-    root = tree.getroot()
+    root = parseXml()
  
     df_base_cols = ["uid", "program"]
     rows = []
@@ -47,15 +53,23 @@ def buildProgramList():
     
     df_base = pd.DataFrame(rows, columns = df_base_cols)
     return(df_base)
+
+def buildAkaList():
+    root = parseXml()
+ 
+    df_base_cols = ["uid", "type", "category", "lastName"]
+    rows = []
+
+    for actor in root.findall('{http://tempuri.org/sdnList.xsd}sdnEntry'):
+
+        uid = actor.find('{http://tempuri.org/sdnList.xsd}uid').text if actor is not None else None
+
+        for actor in actor.findall('{http://tempuri.org/sdnList.xsd}programList'):
+            program = actor.find('{http://tempuri.org/sdnList.xsd}program').text if actor is not None else None
+            rows.append({"uid": uid, "program": program})   #NEED TO ADJUST THE COLS
+
     
+    df_base = pd.DataFrame(rows, columns = df_base_cols)
+    return(print(df_base))
 
-def loadXML():
-
-    url = 'https://www.treasury.gov/ofac/downloads/sdn.xml'
-    resp = requests.get(url)
-
-    with open('resources/sdnOfac.xml', 'wb') as f:
-        f.write(resp.content)
-
-buildProgramList()
-# buildMainDf()
+buildAkaList()
