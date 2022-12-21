b7b50a46d34e8698fc7492a785af7fad91563045
Alysson Amaral <alysson.amaral@outlook.com>
2022-12-21 09:36:44 -0400

    adding new rows

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pandas as pd
 
 
 def loadXML():
-
     url = 'https://www.treasury.gov/ofac/downloads/sdn.xml'
     resp = requests.get(url)
 
@@ -49,15 +48,14 @@ def buildProgramList():
         for actor in actor.findall('{http://tempuri.org/sdnList.xsd}programList'):
             program = actor.find('{http://tempuri.org/sdnList.xsd}program').text if actor is not None else None
             rows.append({"uid": uid, "program": program})
-
-    
+   
     df_base = pd.DataFrame(rows, columns = df_base_cols)
     return(df_base)
 
 def buildAkaList():
     root = parseXml()
  
-    df_base_cols = ["uid", "akaUid","type", "category", "lastName"]
+    df_base_cols = ["uid", "akaUid", "type", "category", "lastName"]
     rows = []
 
     for actor in root.findall('{http://tempuri.org/sdnList.xsd}sdnEntry'):
@@ -66,15 +64,39 @@ def buildAkaList():
 
         for actor in actor.findall('{http://tempuri.org/sdnList.xsd}akaList'):
             for aka in actor.findall('{http://tempuri.org/sdnList.xsd}aka'):
-                akaUid = aka.find('{http://tempuri.org/sdnList.xsd}uid').text if actor is not None else None
-                type = aka.find('{http://tempuri.org/sdnList.xsd}type').text if actor is not None else None
-                category = aka.find('{http://tempuri.org/sdnList.xsd}category').text if actor is not None else None
-                lastName = aka.find('{http://tempuri.org/sdnList.xsd}lastName').text if actor is not None else None
-
+                akaUid = aka.find('{http://tempuri.org/sdnList.xsd}uid').text if aka is not None else None
+                type = aka.find('{http://tempuri.org/sdnList.xsd}type').text if aka is not None else None
+                category = aka.find('{http://tempuri.org/sdnList.xsd}category').text if aka.find('{http://tempuri.org/sdnList.xsd}category') is not None else None
+                lastName = aka.find('{http://tempuri.org/sdnList.xsd}lastName').text if aka.find('{http://tempuri.org/sdnList.xsd}lastName') is not None else None
 
                 rows.append({"uid": uid, "akaUid": akaUid, "type": type, "category": category, "lastName": lastName})   
     
     df_base = pd.DataFrame(rows, columns = df_base_cols)
     return(df_base)
 
-buildAkaList()
+def buildAddressList():
+    root = parseXml()
+ 
+    df_base_cols = ["uid", "addressUid", "city", "country", "address1", "address2", "address3", "postalCode"]
+    rows = []
+
+    for actor in root.findall('{http://tempuri.org/sdnList.xsd}sdnEntry'):
+
+        uid = actor.find('{http://tempuri.org/sdnList.xsd}uid').text if actor.find('{http://tempuri.org/sdnList.xsd}uid') is not None else None
+
+        for actor in actor.findall('{http://tempuri.org/sdnList.xsd}addressList'):
+            for address in actor.findall('{http://tempuri.org/sdnList.xsd}address'):
+                addressUid = address.find('{http://tempuri.org/sdnList.xsd}uid').text if address.find('{http://tempuri.org/sdnList.xsd}uid') is not None else None
+                city = address.find('{http://tempuri.org/sdnList.xsd}city').text if address.find('{http://tempuri.org/sdnList.xsd}city') is not None else None
+                country = address.find('{http://tempuri.org/sdnList.xsd}country').text if address.find('{http://tempuri.org/sdnList.xsd}country') is not None else None
+                address1 = address.find('{http://tempuri.org/sdnList.xsd}address1').text if address.find('{http://tempuri.org/sdnList.xsd}address1') is not None else None
+                address2 = address.find('{http://tempuri.org/sdnList.xsd}address2').text if address.find('{http://tempuri.org/sdnList.xsd}address2') is not None else None
+                address3 = address.find('{http://tempuri.org/sdnList.xsd}address3').text if address.find('{http://tempuri.org/sdnList.xsd}address3') is not None else None
+                postalCode = address.find('{http://tempuri.org/sdnList.xsd}postalCode').text if address.find('{http://tempuri.org/sdnList.xsd}postalCode') is not None else None
+
+                rows.append({"uid": uid, "addressUid": addressUid, "city": city, "country": country, "address1": address1, "address2": address2, "address3": address3, "postalCode": postalCode})   
+    
+    df_base = pd.DataFrame(rows, columns = df_base_cols)
+    return(print(df_base))
+
+buildAddressList()
