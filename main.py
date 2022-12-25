5700ada3075ed9a5f2823c5c4536ae4886e42b1d
Alysson Amaral <alysson.amaral@outlook.com>
2022-12-25 18:33:05 -0400

    adding new functions

--- main.py
+++ main.py
@@ -18,19 +18,18 @@ def parseXml():
 def buildMainDf():
     root = parseXml()
  
-    df_base_cols = ["uid", "lastName", "sdnType"]
+    df_base_cols = ["uid", "lastName", "sdnType", "firstName", "title", "remarks"]
     rows = []
-
-    for child in root:
-        for child in child:
-            print(child.tag)
 
     for actor in root.findall('{http://tempuri.org/sdnList.xsd}sdnEntry'):
         uid = actor.find('{http://tempuri.org/sdnList.xsd}uid').text if actor.find('{http://tempuri.org/sdnList.xsd}uid') is not None else None
+        firstName = actor.find('{http://tempuri.org/sdnList.xsd}firstName').text if actor.find('{http://tempuri.org/sdnList.xsd}firstName') is not None else None
         lastName = actor.find('{http://tempuri.org/sdnList.xsd}lastName').text if actor.find('{http://tempuri.org/sdnList.xsd}lastName') is not None else None
+        title = actor.find('{http://tempuri.org/sdnList.xsd}title').text if actor.find('{http://tempuri.org/sdnList.xsd}title') is not None else None
         sdnType = actor.find('{http://tempuri.org/sdnList.xsd}sdnType').text if actor.find('{http://tempuri.org/sdnList.xsd}sdnType') is not None else None
+        remarks = actor.find('{http://tempuri.org/sdnList.xsd}remarks').text if actor.find('{http://tempuri.org/sdnList.xsd}remarks') is not None else None
 
-        rows.append({"uid": uid, "lastName": lastName, "sdnType": sdnType})
+        rows.append({"uid": uid, "lastName": lastName, "firstName": firstName, "title": title, "sdnType": sdnType, "remarks": remarks})
     
     df_base = pd.DataFrame(rows, columns = df_base_cols)
     return (df_base)
@@ -124,5 +123,125 @@ def buildIdList():
     df_base = pd.DataFrame(rows, columns = df_base_cols)
     return(df_base)
 
+def buildNationalityList():
+    root = parseXml()
 
-buildIdList()
+    df_base_cols = ["uid", "nationalityId", "country", "mainEntry"]
+    rows = []
+
+    for actor in root.findall('{http://tempuri.org/sdnList.xsd}sdnEntry'):
+        
+        uid = actor.find('{http://tempuri.org/sdnList.xsd}uid').text if actor.find('{http://tempuri.org/sdnList.xsd}uid') is not None else None
+
+        for actor in actor.findall('{http://tempuri.org/sdnList.xsd}nationalityList'):
+            for address in actor.findall('{http://tempuri.org/sdnList.xsd}nationality'):
+                nationalityId = address.find('{http://tempuri.org/sdnList.xsd}uid').text if address.find('{http://tempuri.org/sdnList.xsd}uid') is not None else None
+                country = address.find('{http://tempuri.org/sdnList.xsd}country').text if address.find('{http://tempuri.org/sdnList.xsd}country') is not None else None
+                mainEntry = address.find('{http://tempuri.org/sdnList.xsd}mainEntry').text if address.find('{http://tempuri.org/sdnList.xsd}mainEntry') is not None else None
+
+                rows.append({"uid": uid, "nationalityId": nationalityId, "country": country, "mainEntry": mainEntry})   
+    
+    df_base = pd.DataFrame(rows, columns = df_base_cols)
+    return(df_base)
+
+def buildDateOfBirthList():
+    root = parseXml()
+
+    df_base_cols = ["uid", "dateOfBirthId", "dateOfBirth", "mainEntry"]
+    rows = []
+
+    for actor in root.findall('{http://tempuri.org/sdnList.xsd}sdnEntry'):
+        
+        uid = actor.find('{http://tempuri.org/sdnList.xsd}uid').text if actor.find('{http://tempuri.org/sdnList.xsd}uid') is not None else None
+
+        for actor in actor.findall('{http://tempuri.org/sdnList.xsd}dateOfBirthList'):
+            for address in actor.findall('{http://tempuri.org/sdnList.xsd}dateOfBirthItem'):
+                dateOfBirthId = address.find('{http://tempuri.org/sdnList.xsd}uid').text if address.find('{http://tempuri.org/sdnList.xsd}uid') is not None else None
+                dateOfBirth = address.find('{http://tempuri.org/sdnList.xsd}dateOfBirth').text if address.find('{http://tempuri.org/sdnList.xsd}dateOfBirth') is not None else None
+                mainEntry = address.find('{http://tempuri.org/sdnList.xsd}mainEntry').text if address.find('{http://tempuri.org/sdnList.xsd}mainEntry') is not None else None
+
+                rows.append({"uid": uid, "dateOfBirthId": dateOfBirthId, "dateOfBirth": dateOfBirth, "mainEntry": mainEntry})   
+    
+    df_base = pd.DataFrame(rows, columns = df_base_cols)
+    return(df_base)
+
+def buildPlaceOfBirthList():
+    root = parseXml()
+
+    df_base_cols = ["uid", "placeOfBirthId", "placeOfBirth", "mainEntry"]
+    rows = []
+
+    for actor in root.findall('{http://tempuri.org/sdnList.xsd}sdnEntry'):
+        
+        uid = actor.find('{http://tempuri.org/sdnList.xsd}uid').text if actor.find('{http://tempuri.org/sdnList.xsd}uid') is not None else None
+
+        for actor in actor.findall('{http://tempuri.org/sdnList.xsd}placeOfBirthList'):
+            for address in actor.findall('{http://tempuri.org/sdnList.xsd}placeOfBirthItem'):
+                placeOfBirthId = address.find('{http://tempuri.org/sdnList.xsd}uid').text if address.find('{http://tempuri.org/sdnList.xsd}uid') is not None else None
+                placeOfBirth = address.find('{http://tempuri.org/sdnList.xsd}placeOfBirth').text if address.find('{http://tempuri.org/sdnList.xsd}placeOfBirth') is not None else None
+                mainEntry = address.find('{http://tempuri.org/sdnList.xsd}mainEntry').text if address.find('{http://tempuri.org/sdnList.xsd}mainEntry') is not None else None
+
+                rows.append({"uid": uid, "placeOfBirthId": placeOfBirthId, "placeOfBirth": placeOfBirth, "mainEntry": mainEntry})   
+    
+    df_base = pd.DataFrame(rows, columns = df_base_cols)
+    return(df_base)
+
+def buildVesselInfo():
+    root = parseXml()
+
+    df_base_cols = ["uid", "callSign", "vesselType", "vesselFlag", "vesselOwner", "tonnage", "grossRegisteredTonnage"]
+    rows = []
+
+    for actor in root.findall('{http://tempuri.org/sdnList.xsd}sdnEntry'):
+        
+        uid = actor.find('{http://tempuri.org/sdnList.xsd}uid').text if actor.find('{http://tempuri.org/sdnList.xsd}uid') is not None else None
+
+        for address in actor.findall('{http://tempuri.org/sdnList.xsd}vesselInfo'):
+            callSign = address.find('{http://tempuri.org/sdnList.xsd}callSign').text if address.find('{http://tempuri.org/sdnList.xsd}callSign') is not None else None
+            vesselType = address.find('{http://tempuri.org/sdnList.xsd}vesselType').text if address.find('{http://tempuri.org/sdnList.xsd}vesselType') is not None else None
+            vesselFlag = address.find('{http://tempuri.org/sdnList.xsd}vesselFlag').text if address.find('{http://tempuri.org/sdnList.xsd}vesselFlag') is not None else None
+            vesselOwner = address.find('{http://tempuri.org/sdnList.xsd}vesselOwner').text if address.find('{http://tempuri.org/sdnList.xsd}vesselOwner') is not None else None
+            tonnage = address.find('{http://tempuri.org/sdnList.xsd}tonnage').text if address.find('{http://tempuri.org/sdnList.xsd}tonnage') is not None else None
+            grossRegisteredTonnage = address.find('{http://tempuri.org/sdnList.xsd}grossRegisteredTonnage').text if address.find('{http://tempuri.org/sdnList.xsd}grossRegisteredTonnage') is not None else None
+
+            rows.append({"uid": uid, "callSign": callSign, "vesselType": vesselType, "vesselFlag": vesselFlag, "vesselOwner": vesselOwner, "tonnage": tonnage, "grossRegisteredTonnage": grossRegisteredTonnage})   
+    
+    df_base = pd.DataFrame(rows, columns = df_base_cols)
+    return(df_base)
+
+def searchPatterns():
+    root = parseXml()
+    df_base_cols = ["tag"]
+    rows = []
+
+    # for child in root:
+    #     for child in child:
+    #         rows.append({"tag": child.tag})  
+
+    for actor in root.findall('{http://tempuri.org/sdnList.xsd}sdnEntry'):
+        for actor in actor.findall('{http://tempuri.org/sdnList.xsd}placeOfBirthList'):
+            for address in actor.findall('{http://tempuri.org/sdnList.xsd}placeOfBirthItem'):
+                rows.append({"tag": address.tag})
+
+# ['{http://tempuri.org/sdnList.xsd}Publish_Date'
+#  '{http://tempuri.org/sdnList.xsd}Record_Count'
+#  '{http://tempuri.org/sdnList.xsd}uid'
+#  '{http://tempuri.org/sdnList.xsd}lastName'
+#  '{http://tempuri.org/sdnList.xsd}sdnType'
+#  '{http://tempuri.org/sdnList.xsd}programList'
+#  '{http://tempuri.org/sdnList.xsd}akaList'
+#  '{http://tempuri.org/sdnList.xsd}addressList'
+#  '{http://tempuri.org/sdnList.xsd}idList'
+#  '{http://tempuri.org/sdnList.xsd}firstName'
+#  '{http://tempuri.org/sdnList.xsd}title'
+#  '{http://tempuri.org/sdnList.xsd}dateOfBirthList'
+#  '{http://tempuri.org/sdnList.xsd}placeOfBirthList'
+#  '{http://tempuri.org/sdnList.xsd}nationalityList'
+#  '{http://tempuri.org/sdnList.xsd}remarks'
+#  '{http://tempuri.org/sdnList.xsd}vesselInfo'
+#  '{http://tempuri.org/sdnList.xsd}citizenshipList']
+
+    df_base = pd.DataFrame(rows, columns = df_base_cols)
+    return(print(df_base['tag'].unique()))
+
+buildVesselInfo()
