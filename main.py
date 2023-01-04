89f2bbff237a6a26ab1f8ad19de0f4c9327d31ce
Alysson Amaral <alysson.amaral@outlook.com>
2023-01-03 23:29:28 -0400

    adding first flask route

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import ofac_to_df as ofac
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/search_ofac',methods=['GET'])
+def search_ofac():
+    payload = request.get_json()
+    print(payload)
+    return 'Aopa'
+
+
+app.run(port=5000,host='localhost',debug=True)
